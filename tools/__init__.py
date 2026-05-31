79b57aeffabcaca0b1dd5dc5cc58aaf3d45f4534
Sharan G S <sharansassrini@gmail.com>
2026-05-31 23:49:27 +0530

    feat(tools): export portfolio rebalancer functions from tools package

--- tools/__init__.py
+++ tools/__init__.py
@@ -40,10 +40,15 @@ __all__ = [
     # backtesting
     "format_backtest_report",
     "run_crossover_backtest",
+    # portfolio rebalancing
+    "compute_portfolio_rebalancing",
+    "format_rebalance_report",
 ]
 
 from .backtest_simulator import format_backtest_report, run_crossover_backtest
 from .momentum import compute_cci, compute_roc, compute_williams_r, get_momentum_summary
+from .portfolio_rebalancer import compute_portfolio_rebalancing, format_rebalance_report
 from .portfolio_summary import compute_portfolio_summary, format_portfolio_summary
+
 
 
